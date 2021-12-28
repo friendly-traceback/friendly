c84115c0b532d56d60d9c1ef509f173eb1e52847
André Roberge <andre.roberge@gmail.com>
2021-12-27 22:41:49 -0400

    Getting previous formatter environment correctly for IPython family

--- friendly/ipython_common/settings.py
+++ friendly/ipython_common/settings.py
@@ -7,7 +7,7 @@ def init_configuration(default_formatter="dark"):
     """Initialises the formatter using saved configurations for the
     current environment.
     """
-    if configuration.has_environment("configuration.ENVIRONMENT"):
+    if configuration.has_environment(configuration.ENVIRONMENT):
         formatter = configuration.read(option="formatter")
         background = configuration.read(option="background")
         color_system = configuration.read(option="color_system")
@@ -21,7 +21,7 @@ def init_configuration(default_formatter="dark"):
     else:
         set_formatter(default_formatter)
 
-    if configuration.has_environment("configuration.ENVIRONMENT"):
+    if configuration.has_environment(configuration.ENVIRONMENT):
         _ipython_prompt = configuration.read(option="ipython_prompt")
         if _ipython_prompt in [None, "True"]:
             config.session.ipython_prompt = True
