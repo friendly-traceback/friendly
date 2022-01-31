534a6c45104f5a35470aed17a3bc9d1794dc017f
André Roberge <andre.roberge@gmail.com>
2022-01-30 22:03:49 -0400

    Better highlighting for Rich.

--- friendly/theme/friendly_rich.py
+++ friendly/theme/friendly_rich.py
@@ -3,7 +3,7 @@
 All Rich-related imports and redefinitions are done here.
 
 """
-import re
+import inspect
 import sys
 
 from ..utils import get_highlighting_range
@@ -16,9 +16,18 @@ from rich.text import Text
 from rich.theme import Theme
 
 from pygments import styles
-from pygments.token import Error, Number, Operator
+from pygments.token import (
+    Comment,
+    Error,
+    Generic,
+    Keyword,
+    Name,
+    Number,
+    Operator,
+    String,
+)
 from friendly_styles import friendly_light, friendly_dark
-from friendly_traceback import debug_helper
+from friendly_traceback import token_utils
 
 # When friendly is imported in environments that have previously
 # imported Pygments, the styles defined in friendly_styles do not
@@ -38,40 +47,97 @@ light_background_theme = Theme(friendly_light.friendly_style)
 USE_CARETS = False
 
 
+def is_builtin(string):
+    try:
+        return inspect.isbuiltin(eval(string))
+    except:  # noqa
+        return False
+
+
+def is_exception(string):
+    try:
+        return issubclass(eval(string), BaseException)
+    except:  # noqa
+        return False
+
+
 def highlight_line(line, line_parts, theme):
     error_style = theme.styles[Error]
     background = theme.background_color
     operator_style = f"{theme.styles[Operator]} on {background}"
     number_style = f"{theme.styles[Number]} on {background}"
+    code_style = f"{theme.styles[Name]} on {background}"
+    keyword_style = f"{theme.styles[Keyword]} on {background}"
+    constant_style = f"{theme.styles[Keyword.Constant]} on {background}"
+    comment_style = f"{theme.styles[Comment]} on {background}"
+    builtin_style = f"{theme.styles[Name.Builtin]} on {background}"
+    exception_style = f"{theme.styles[Generic.Error]} on {background}"
+    string_style = f"{theme.styles[String]} on {background}"
 
     bg, fg = error_style.split(" ")
     bg = bg.split(":")[1]
     error_style = f"{fg} on {bg}"
-    has_line_number = re.match(r"^\s*\d+\s*:", line) or re.match(
-        r"^\s*-->\s*\d+\s*:", line
-    )
-    colon_position = line.find(":")
     highlighting = False
     text = None
+    tokens = token_utils.tokenize(line)
+    last_column = 0
     for begin, end in line_parts:
         if highlighting:
             part = Text(line[begin:end], style=error_style)
-        elif has_line_number and begin < colon_position:
-            begin_line = line[begin : colon_position + 1]
-            arrow_position = begin_line.find("-->")
-            if arrow_position != -1:
-                part = Text(line[begin : arrow_position + 3], style=operator_style)
-                part.append(
-                    Text(line[arrow_position + 3 : colon_position], style=number_style)
-                )
-            else:
-                part = Text(line[begin:colon_position], style=number_style)
-            part.append(
-                Text(line[colon_position : colon_position + 1], style=operator_style)
-            )
-            part.append(Text(line[colon_position + 1 : end], style="markdown.code"))
+            last_column = end
         else:
-            part = Text(line[begin:end], style="markdown.code")
+            part = None
+            for token in tokens:
+                if token.start_col < begin:
+                    continue
+                if token.start_col >= end:
+                    if part is None:
+                        part = Text(
+                            " " * (token.start_col - last_column), style=code_style
+                        )
+                    else:
+                        part.append(
+                            Text(
+                                " " * (token.start_col - last_column), style=code_style
+                            )
+                        )
+                    last_column = token.start_col
+                    break
+                if token.start_col > last_column:
+                    if part is None:
+                        part = Text(
+                            " " * (token.start_col - last_column), style=code_style
+                        )
+                    else:
+                        part.append(
+                            Text(
+                                " " * (token.start_col - last_column), style=code_style
+                            )
+                        )
+                last_column = token.end_col
+                if token.is_keyword():
+                    if token.string in ["True", "False", "None"]:
+                        sub_part = Text(token.string, style=constant_style)
+                    else:
+                        sub_part = Text(token.string, style=keyword_style)
+                elif is_builtin(token.string):
+                    sub_part = Text(token.string, style=builtin_style)
+                elif is_exception(token.string):
+                    sub_part = Text(token.string, style=exception_style)
+                elif token.is_comment():
+                    sub_part = Text(token.string, style=comment_style)
+                elif token.is_number():
+                    sub_part = Text(token.string, style=number_style)
+                elif token.is_operator():
+                    sub_part = Text(token.string, style=operator_style)
+                elif token.is_string():
+                    sub_part = Text(token.string, style=string_style)
+                else:
+                    sub_part = Text(token.string, style=code_style)
+                if part is None:
+                    part = sub_part
+                else:
+                    part.append(sub_part)
         if text is None:
             text = part
         else:
@@ -101,12 +167,7 @@ def init_console(theme=friendly_dark, color_system="auto", force_jupyter=None):
 
         lines = code.split("\n")
         error_lines = get_highlighting_range(lines)
-        if (
-            debug_helper.DEBUG
-            and not USE_CARETS
-            and self.lexer_name == "python"
-            and error_lines
-        ):
+        if not USE_CARETS and self.lexer_name == "python" and error_lines:
             for index, line in enumerate(lines):
                 if index in error_lines:
                     continue
