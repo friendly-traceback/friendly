fd6a9952a2769b0e0049ab3f5500ecbeeee08b96
André Roberge <andre.roberge@gmail.com>
2022-10-15 15:31:29 -0300

    Ensuring that the test pass even if change the language in a previous session.

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -4,14 +4,23 @@ from friendly_traceback.config import session
 
 
 def test_basic():
+    # Make sure that we always get the same results no matter
+    # what the language settings have been set to
+    lang = friendly.get_lang()
+    friendly.set_lang("en")
+
     friendly.set_formatter("dark", color_system="truecolor")
     console = session.console
     with console.capture() as capture:
         try:
-            a = b
+            a = b  # noqa
         except NameError:
             friendly_traceback.explain_traceback()
     str_output = capture.get()
-    with open("tests/basic_text.out",encoding="utf8") as f:
+
+    with open("tests/basic_text.out", encoding="utf8") as f:
         result = f.read()
     assert str_output == result
+
+    # Restore the original settings
+    friendly.set_lang(lang)
