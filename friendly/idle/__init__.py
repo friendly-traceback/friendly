0e81f6ae8c28ec60794b164c230c83250920626f
André Roberge <andre.roberge@gmail.com>
2022-10-15 18:44:27 -0300

    Added information about warnings to be suppressed for friendly_idle

--- friendly/idle/__init__.py
+++ friendly/idle/__init__.py
@@ -3,14 +3,23 @@ from .main import *  # noqa
 from .main import install
 from friendly import settings
 from friendly_traceback.console_helpers import friendly_tb
-from friendly_traceback import config
+from friendly_traceback import config, add_ignored_warnings
 
 settings.ENVIRONMENT = "IDLE"
 
+
+def do_not_show_warnings(warning_instance, warning_type, filename, lineno):
+    # These warnings occur with friendly_idle
+    return warning_type == ImportWarning and str(warning_instance) in {
+        "PatchingFinder.find_spec() not found; falling back to find_module()",
+        "PatchingLoader.exec_module() not found; falling back to load_module()",
+    }
+
+
+add_ignored_warnings(do_not_show_warnings)
+
 __all__ = list(helpers)  # noqa
-__all__.append("run")  # noqa
-__all__.append("start_console")  # noqa
-__all__.append("Friendly")  # noqa
+__all__.extend(("run", "start_console", "Friendly"))
 __all__.remove("disable")
 __all__.remove("enable")
 install()
