ef607f995fb43ac9db84ea767c61dc4ce3def809
André Roberge <andre.roberge@gmail.com>
2022-10-15 18:21:17 -0300

    Using new custom ignore warning functions.

--- friendly/ipython_common/__init__.py
+++ friendly/ipython_common/__init__.py
@@ -1,1 +1,9 @@
-pass
+from friendly_traceback import add_ignored_warnings
+
+
+def _do_not_show_warning(warning_instance, warning_type, filename, lineno) -> bool:
+    return filename == "<>"
+
+
+add_ignored_warnings(_do_not_show_warning)
+del add_ignored_warnings
